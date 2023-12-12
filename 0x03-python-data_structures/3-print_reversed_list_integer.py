b89eacade7f27fae0dae105c7c0ec7a01acac3d7
raykiwara <raykiwara10@gmail.com>
2023-12-12 11:52:12 +0000

    updates

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
+    """length = len(my_list)
     print("Length before loop: {}".format(length))
     for i in range(length):
         my_list[i] = my_list[length - 1]
         #length = length - 1
         print("Length: {}".format(length))
         print("{}".format(my_list[i]))
-        length = length - 1
+        length = length - 1"""
+    my_list.reverse()
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy = my_list.copy()
+
+    if idx < 0:
+        return copy
+    elif idx >= len(copy):
+        return copy
+    else:
+        copy[idx] = element
+    print("{}".format(copy))
