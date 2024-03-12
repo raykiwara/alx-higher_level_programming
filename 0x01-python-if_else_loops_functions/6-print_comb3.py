6d2db4021e398a62233c7b160424f8e0f0a0a034
raykiwara <raykiwara10@gmail.com>
2024-03-12 19:14:42 +0000

    Print comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i < j and i != j:
             if (i < 8):
-                print(f"{i:d}{j:d}", end=", ")
+                print(f"{i}{j}".format(i, j), end=", ")
             else:
-                print(f"{i:d}{j:d}")
+                print(f"{i}{j}".format(i, j))
