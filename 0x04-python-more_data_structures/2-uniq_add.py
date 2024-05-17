4e0d1c2d797dc053ac0e3ceb7d2768013f8c87d7
raykiwara <raykiwara10@gmail.com>
2024-05-17 14:54:07 +0000

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    length = len(my_list)
-    j = 0
-    for i in my_list:
-        if my_list:
-            if (my_list[i] < my_list[i + 1]):
-                j += my_list[i + 1]
-        length -= length
-    return j
+    uniq = set(my_list)
+    uniqlist = list(uniq)
+    sum = 0
+    for i in uniqlist:
+        sum += uniqlist[i - 1]
+    return sum
