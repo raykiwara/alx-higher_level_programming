268ee7afaef509d99f2d25c1483040350fe0cdd8
raykiwara <raykiwara10@gmail.com>
2024-05-20 10:09:09 +0000

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,6 @@ def uniq_add(my_list=[]):
     uniq = set(my_list)
     uniqlist = list(uniq)
     sum = 0
-    for i in uniqlist:
+    for i in range(len(uniqlist)):
         sum += uniqlist[i - 1]
     return sum
