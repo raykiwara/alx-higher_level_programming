f29359bf0bac1f541ade68843c64251a1d9e711e
raykiwara <raykiwara10@gmail.com>
2024-05-28 09:31:13 +0000

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+        print()
+        return x
+    except IndexError:
+        if x > 5:
+            i == 5
+        print()
+        return i
+        pass
